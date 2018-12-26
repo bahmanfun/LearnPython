0bc8983568ac3fae267f8b4a37fe4b06f8e99179
bahmanfun <bahmanfun>
2018-12-27 01:46:42 +0200

    add .py files

---
+++ pitza_shoa.py
@@ -0,0 +1,17 @@
+#input method
+shoa=float(input("andaze shoa: "))
+
+
+pi=3.14 #verywhere knows
+
+
+#calculating mohit and print result
+mohit=(shoa*2)*pi
+print("pizza mohit is : ", mohit)
+
+
+#calculating masahat and prit result
+masahat=(shoa*shoa)*pi
+print("pizza masahat is : ",masahat)
+
+
---
+++ test.py
@@ -0,0 +1,29 @@
+#input method
+print("           ")
+print("           ")
+tul=input("meqdare tul ra vared nemaid : ")
+print("           ")
+print("           ")
+
+#convert to float
+tulfloat=float(tul)
+
+#input arz method
+print("           ")
+print("           ")
+arz=input("megdare arz ra vared nemaiid : ")
+print("           ")
+print("           ")
+
+#convert to float
+arzfloat=float(arz)
+
+#calculating
+shoa=tulfloat*arzfloat
+
+#print result
+print("           ")
+print("           ")
+print("masahate mostatil",shoa,"ast")
+print("           ")
+print("           ")
